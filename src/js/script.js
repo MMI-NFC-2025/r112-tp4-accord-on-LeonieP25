cb791ca7e564360fe263dea7623783e799206662
LeonieP25 <leonie.pruniaux@edu.univ-fcomte.fr>
2025-11-26 14:00:04 +0100

    feat: referme les détails suite à un clic sur un summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 // *** ACCORDEON ***
+
+const accordeon = document.querySelector(".accordeon");
+const allDetails = accordeon.querySelectorAll("details");
+
+allDetails.forEach((details) => {
+  details.addEventListener("toggle", () => {
+    if (details.open) {
+      allDetails.forEach((det) => {
+        if (det !== details) {
+          det.open = false;
+        }
+      });
+    }
+  });
+});
